1cebca96f4b0cf683cc2883a2a4149dae95ac259
luisenjs <107270563+luisenjs@users.noreply.github.com>
2024-06-03 10:55:12 -0500

    Eventos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-let loaded = (eventLoaded) => {
+/*let loaded = (eventLoaded) => {
 
     window.alert("landing page loaded");
     console.log(eventLoaded);
     debugger;
 
-}
+}*/
+
+let loaded = ( eventLoaded ) => {
+
+    let myform = document.getElementById('formulario');
+    
+    myform.addEventListener('submit', ( eventSubmit ) => { 
+  
+      debugger;
+  
+    })
+  
+  }
 
 window.addEventListener("DOMContentLoaded", loaded);
+
