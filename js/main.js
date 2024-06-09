8207225aa3740ee9bce1386cccbfd4884f432db9
JaraSanchezLuis <107270563+luisenjs@users.noreply.github.com>
2024-06-08 23:03:15 -0500

    Cambios en la interfaz e implementación de fetch

--- js/main.js
+++ js/main.js
@@ -1,22 +1,36 @@
-/*let loaded = (eventLoaded) => {
-
-    window.alert("landing page loaded");
-    console.log(eventLoaded);
-    debugger;
-
-}*/
-
-let loaded = ( eventLoaded ) => {
-
-    let myform = document.getElementById('formulario');
-    
-    myform.addEventListener('submit', ( eventSubmit ) => { 
-  
-      debugger;
-  
+let loaded = (eventLoaded) => {
+  /*window.alert("landing page loaded");
+  console.log( eventLoaded );*/
+  let myform = document.getElementById("formulario");
+  myform.addEventListener('submit', (eventSubmit) => {
+    eventSubmit.preventDefault();
+    let nombre = document.getElementById("nombre-form").value;
+    let correo = document.getElementById("correo-form").value;
+    if (nombre.length == 0) {
+      alert("Ingrese su nombre por favor");
+      return;
+    }
+    if (correo.length == 0) {
+      alert("Ingrese un correo por favor");
+      return;
+    }
+    const datos = {
+      nombre: nombre,
+      correo: correo
+    }
+    fetch("https://dawm-82af5-default-rtdb.firebaseio.com/coleccion.json", {
+      method: "POST",
+      body: JSON.stringify(datos),
+      headers: {
+        "Content-Type": "application/json"
+      }
     })
-  
-  }
+      .then(respuesta => respuesta.json())
+      .then(datos => {
+        console.log(datos);
+      })
+      .catch(error => console.error(error))
+  });
+}
 
 window.addEventListener("DOMContentLoaded", loaded);
-
