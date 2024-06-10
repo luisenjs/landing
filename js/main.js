657b18759c353d4d81d28912af943293b4fe9737
luisenjs <107270563+luisenjs@users.noreply.github.com>
2024-06-10 10:57:29 -0500

    GET y mapas para presentación de datos

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 let loaded = (eventLoaded) => {
-  /*window.alert("landing page loaded");
-  console.log( eventLoaded );*/
   let myform = document.getElementById("formulario");
   myform.addEventListener('submit', (eventSubmit) => {
     eventSubmit.preventDefault();
     let nombre = document.getElementById("nombre-form").value;
     let correo = document.getElementById("correo-form").value;
+    let interes = document.getElementById("interes-form").value;
     if (nombre.length == 0) {
       alert("Ingrese su nombre por favor");
       return;
@@ -16,7 +15,8 @@ let loaded = (eventLoaded) => {
     }
     const datos = {
       nombre: nombre,
-      correo: correo
+      correo: correo,
+      interes: interes
     }
     fetch("https://dawm-82af5-default-rtdb.firebaseio.com/coleccion.json", {
       method: "POST",
@@ -34,3 +34,24 @@ let loaded = (eventLoaded) => {
 }
 
 window.addEventListener("DOMContentLoaded", loaded);
+
+async function obtenerDatos() {
+  const url = "https://dawm-82af5-default-rtdb.firebaseio.com/coleccion.json";
+  const respuesta = await fetch(url);
+  debugger;
+  if (!respuesta.ok) {
+    console.error("Error:", respuesta.status);
+    return;
+  }
+  const datos = await respuesta.json();
+  let interesTotales = new Map();
+  for (var key in datos) {
+    let elementos = datos[key];
+    let interes = elementos["interes"];
+    let conteo = interesTotales.has(interes)?interesTotales.get(interes)+1:1;
+    interesTotales.set(interes,conteo);
+  }
+  console.log(interesTotales);
+}
+
+obtenerDatos();
